560f747f93c5e9137169767422d7cc2b8ec5ff80
Nhan Cao <nhancv92@gmail.com>
2017-12-20 17:23:06 +0700

    update

--- App/Containers/Profile/Profile.Action.js
+++ App/Containers/Profile/Profile.Action.js
@@ -1,34 +1,29 @@
-import { call, put } from 'redux-saga/effects'
-import { path } from 'ramda'
+import {call, put} from 'redux-saga/effects'
 
 import {createActions} from 'reduxsauce'
 
 /* ------------- Types and Action Creators ------------- */
 const {Types, Creators} = createActions({
-  userRequest:['username'],
+  userRequest: ['username'],
   request: ['request'],
   success: ['data'],
   failure: ['error']
 });
 
 export const ProfileType = Types;
-export const ProfileAction =  Creators;
+export const ProfileAction = Creators;
 export const ProfileFunction = {
   getData,
   getUser
 };
 
-function * getData (api, action) {
+function* getData(api, action) {
   const {request} = action;
   const response = yield call(api.getData, request);
-  if (!response.ok) {
-    yield put(ProfileAction.success(response))
-  } else {
-    yield put(ProfileAction.failure())
-  }
+  yield put(ProfileAction.success(response))
 }
 
-function * getUser (api, action) {
+function* getUser(api, action) {
   const {username} = action;
   const response = yield call(api.getUser, username);
   if (response.ok && response.status === 200) {
