3ec3f29edf623f6bf753aeeded71dde9ddde428c
Nhan Cao <nhancv92@gmail.com>
2018-05-22 09:54:34 +0700

    update

--- App/Config/AppConfig.js
+++ App/Config/AppConfig.js
@@ -1,6 +1,8 @@
 // Simple React Native specific changes
 
-import '../I18n/I18n'
+import I18n from '../I18n/I18n'
+
+I18n.initial()
 
 export default {
   // font scaling override - RN default is on
